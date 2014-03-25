c5ccdf8318db30e35240d9f3392e1a81c477f19b
sienkiew <sienkiew@fe389314-cf27-0410-b35b-8c050e845b92>
2014-03-25 14:53:08 +0000

    mod for d2to1 install
    git-svn-id: https://svn.stsci.edu/svn/ssb/stsci_python/stsci.sphere/trunk@30670 fe389314-cf27-0410-b35b-8c050e845b92

--- setup.py
+++ setup.py
@@ -1,45 +1,19 @@
 #!/usr/bin/env python
 
-CONTACT = "Michael Droettboom"
-EMAIL = "help@stsci.edu"
-VERSION = "0.1"
-
-from distutils.core import setup, Extension
-import sys
-
 try:
-    import numpy
+    from setuptools import setup
 except ImportError:
-    print("numpy must be installed to build sphere.")
-    print("ABORTING.")
-    raise
-
-major, minor, rest = numpy.__version__.split(".", 2)
-if (int(major), int(minor)) < (1, 4):
-    print("numpy version 1.4 or later must be installed to build pywcs.")
-    print("ABORTING.")
-    raise ImportError
-
-try:
-    numpy_include = numpy.get_include()
-except AttributeError:
-    numpy_include = numpy.get_numpy_include()
-
-extensions = [
-    Extension('sphere.math_util',
-              ['src/math_util.c'],
-              include_dirs = [numpy_include],
-              libraries = ['m'])
-    ]
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
 
 setup(
-    name =           'sphere',
-    version =        VERSION,
-    description =    "Python based tools for spherical geometry",
-    author =         CONTACT,
-    author_email =   EMAIL,
-    packages =       ['sphere', 'sphere.test'],
-    package_dir =    {'sphere': 'lib', 'sphere.test': 'lib/test'},
-    package_data =   {'sphere.test': ['data/*.fits', 'data/*.fits.gz']},
-    ext_modules =    extensions
-    )
+    setup_requires=['d2to1>=0.2.5', 'stsci.distutils>=0.3.dev'],
+    namespace_packages=['stsci'], packages=['stsci'],
+    d2to1=True,
+    use_2to3=True,
+    zip_safe=False
+)
+
+#     package_data =   {'stsci.sphere.test': ['data/*.fits', 'data/*.fits.gz']},
+
